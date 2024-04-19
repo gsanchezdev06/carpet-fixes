f81ce94b58719bf20245a438173ca42723ae4a37
Crystal_0404 <120556221+Crystal0404@users.noreply.github.com>
2024-04-19 19:27:32 +0800

    fix reIntroduceTrapdoorUpdateSkipping (#171)
    * fix reIntroduceTrapdoorUpdateSkipping
    
    * One line is missing..

--- src/main/java/carpetfixes/mixins/reIntroduced/RedstoneWireBlock_skipperMixin.java
+++ src/main/java/carpetfixes/mixins/reIntroduced/RedstoneWireBlock_skipperMixin.java
@@ -1,11 +1,13 @@
 package carpetfixes.mixins.reIntroduced;
 
 import carpetfixes.CFSettings;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.RedstoneWireBlock;
 import net.minecraft.block.TrapdoorBlock;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 
 @Mixin(RedstoneWireBlock.class)
@@ -19,5 +21,18 @@ public class RedstoneWireBlock_skipperMixin {
     )
     private boolean cf$trapdoorUpdateSkipping(Object obj, Operation<Boolean> original) {
         return !CFSettings.reIntroduceTrapdoorUpdateSkipping && original.call(obj);
+    }
+
+    
+    @ModifyExpressionValue(
+            method = "getStateForNeighborUpdate",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/RedstoneWireBlock;canRunOnTop(Lnet/minecraft/world/BlockView;" +
+                            "Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"
+            )
+    )
+    private boolean cf$trapdoorUpdateSkipping(boolean original) {
+        return CFSettings.reIntroduceTrapdoorUpdateSkipping || original;
     }
 }
